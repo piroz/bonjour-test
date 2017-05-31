ab7b288cd7fe50bcfda21db7f297cbfdcbcec8cc
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-05-31 18:37:22 +0900

    fix load.js plist.js

--- load.js
+++ load.js
@@ -1,12 +1,27 @@
 const cp = require("child_process");
-var plist_path = __dirname + "/me.muraka.bonjour-test.plist";
+const fs = require("fs-extra");
 
-switch (process.argv[1]) {
+var plist_path = __dirname + "/me.muraka.bonjour-test.plist";
+var home_plist_path = process.env.HOME + "/Library/LaunchAgents/me.muraka.bonjour-test.plist";
+
+switch (process.argv[2]) {
     case '--unload':
+        console.log("unload");
         cp.spawn("launchctl", ["unload", plist_path], { detached: true });
+        fs.unlink(home_plist_path, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
         break;
     case '--load':
     default:
-        cp.spawn("launchctl", ["load", plist_path], { detached: true });
+        console.log("load");
+        fs.copy(plist_path, home_plist_path, (err) => {
+            if (err) {
+                throw err;
+            }
+            cp.spawn("launchctl", ["load", plist_path], { detached: true });
+        });
         break;
 }
--- plist.js
+++ plist.js
@@ -1,26 +1,17 @@
 const fs = require("fs");
-var plist = `<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN"
-          "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>Label</key>
-    <string>me.muraka.bonjour-test.plist</string>
-    <key>ProgramArguments</key>
-    <array>
-      <string>${__dirname}/bin/bonjour-test-daemon.sh</string>
-    </array>
-    <key>RunAtLoad</key>
-    <true/>
-    <key>StandardErrorPath</key>
-    <string>/dev/null</string>
-    <key>StandardOutPath</key>
-    <string>/dev/null</string>
-  </dict>
-</plist>
-`;
+const plist = require('plist');
 
-fs.writeFile(__dirname + "/me.muraka.bonjour-test.plist", plist, (err) => {
+var json = {
+  Label: "me.muraka.bonjour-test.plist",
+  "ProgramArguments": [
+    `${__dirname}/bin/bonjour-test-daemon.sh`
+  ],
+  RunAtLoad: true,
+  StandardErrorPath: "/dev/null",
+  StandardOutPath: "/dev/null"
+};
+
+fs.writeFile(__dirname + "/me.muraka.bonjour-test.plist", plist.build(json), (err) => {
   if (err) {
     throw err;
   }
